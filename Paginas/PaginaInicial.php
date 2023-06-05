fcb843bbae42c20350257ed244f9a7b6aab476f6
ja1ja1ja1 <joaolucasdsouza@gmail.com>
2023-06-05 14:58:27 -0300

    mudei o nome da pagina para pagina inicial

---
+++ Paginas/PaginaInicial.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Modelo da Página Inicial será baseado no blog do google -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="" alt=""> 
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+    
+</body>
+</html>
