3311944996e439be25196da4c92a3b2e6f8c2648
ja1ja1ja1 <joaolucasdsouza@gmail.com>
2023-06-07 12:35:42 -0300

    Estrutura do site

---
+++ Paginas/PaginaInicial.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Modelo da Página Inicial será baseado no blog do google -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <header>
+        <nav id>
+            <img src="" alt=""> 
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+    <img src="" alt=""> 
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+    </footer>
+    
+</body>
+</html>
